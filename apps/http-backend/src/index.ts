68e8d4361c1d370cdc23b547e3d71b79f3b96b75
Silver <rajat.saraswat.0409@gmail.com>
2025-08-21 22:43:47 +0530

    Update index.ts, added the password hashing

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,10 +2,34 @@ import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "./config";
 import { middleware } from "./middleware";
+import bcrypt from "bcrypt";
 const app = express()
 const port = 3001;
-app.post("/signup" , (req:Request , res:Response)=>{
-    //db call
+app.use(express.json());
+
+app.post("/signup" , async (req:Request , res:Response)=>{
+
+    const { username, password } = req.body;
+
+    
+  if (!username || !password) {
+    return res.status(400).json({ error: "Username and password are required" });
+  }
+
+  const existingUser = users.find((u) => u.username === username);
+  if (existingUser) {
+    return res.status(400).json({ error: "User already exists" });
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const newUser: User = {
+    id: userIdCounter++,
+    username,
+    passwordHash,
+  };
+
+    //db call -> use postgres :)
     res.json("signup page")
 })
 app.post("/signin" , (req:Request,res:Response)=>{
