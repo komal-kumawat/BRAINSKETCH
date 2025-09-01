6fed6d1d4f1d98842deb13f89d800196e1c45b5b
komal kumawat <komalkumawat@MacBook-Air.local>
2025-09-01 12:18:23 +0530

    added common folder:

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,13 +1,25 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
+const {CreateUserSchema , SigninSchema , CreateRoomSchema} = require("@repo/common/types");
 const JWT_SECRET  =require("@repo/backend-common/config");
 const app = express();
 
 app.post("/signup" , (req:Request , res:Response)=>{
-    res.json({
-        userId:"123"
-    })
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.status(500).json({message:"incorrect input"});
+        return ;
+    }
+    try{
+        
+
+
+    }
+    catch(err){
+        res.status(500).json({message:"error in signin"})
+    }
+    
 })
 
 app.post("/signin" , (req:Request , res:Response)=>{
